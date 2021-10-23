bd7b54a00d30b155dc36f907098737bd36f825fd
KirKorsh <47732960+KirKorsh@users.noreply.github.com>
2021-10-24 02:51:57 +0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -2,14 +2,11 @@ function f() {
     let price = document.getElementsByName("price");
     let c = document.getElementsByName("count");
     let r = document.getElementById("result");
-    let s = price[0].value*c[0].value;
-    if (s!==s) 
-	{
+    let s = price[0].value * c[0].value;
+    if (s.isNaN()) {
         r.innerHTML = "Ошибка";
-    }
-    else 
-	{
-        r.innerHTML =  s;
+    } else {
+        r.innerHTML = s;
     }
     return false;
-  }
+}
